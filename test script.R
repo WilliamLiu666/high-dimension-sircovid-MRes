2bdde226d1e6058a448791c6b8bc938bba8f72cb
WilliamLiu666 <cyfyliu@gmail.com>
2022-11-07 09:19:25 +0000

    clear draft

---
+++ test script.R
@@ -0,0 +1,18 @@
+#This script is purely used for testing on the student's pc.
+
+setwd("C:/Users/WILL LIU/Desktop/Data Science/high-dimension-sircovid-MRes")
+
+orderly::orderly_run(
+  "vaccine_delay_fits_data"
+)
+
+orderly::orderly_develop_start(
+  "vaccine_delay_parameters_fits",
+  parameters = list(deterministic = TRUE),
+  use_draft = "newer")
+
+orderly::orderly_develop_start('vaccine_delay_fits',
+                     parameters = list(region = "london",
+                                       short_run = TRUE,
+                                       deterministic = TRUE),
+                     use_draft = "newer")
